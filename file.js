79dbe717542177075791b692f22e9d11ab5791d9
MonikaPandian <panmonikmm@gmail.com>
2022-09-07 17:58:25 +0530

    node-mongo

---
+++ file.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+const quote = "Life is either a daring adventure or nothing at all."
+const quote2 = "Whoever is happy will make others happy too."
+
+// fs.writeFile("./quote.ppt",quote,(err)=>{
+//     console.log("completed writing quote.ppt")
+// })
+
+// const[,,n]=process.argv;
+
+// for(i=1;i<=n;i++){
+// fs.writeFile(`./Backup/text-${i}.html`,quote2,(err)=>{
+//     console.log(`completed writing text-${i}.html`)
+// })
+// }
+
---
+++ sum.js
@@ -0,0 +1,7 @@
+console.log("Hello world....!!!!")
+
+
+const sum =(m,n)=>{return m+n}
+const[,,m,n]=process.argv;
+
+console.log(sum(m,n));
